9736fd825f3ee4679f320f14b36fbf7f802983d8
NR3hman <natasha.rehman@hobslegaldocs.com>
2015-08-15 16:33:15 +0100

    #003

--- cdrv4/cdrv4/Forms/AddData.Designer.cs
+++ cdrv4/cdrv4/Forms/AddData.Designer.cs
@@ -28,21 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_SelectFile = new System.Windows.Forms.Button();
+            this.btn_Parse = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb_selectedFile = new System.Windows.Forms.TextBox();
+            this.txb_CaseName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btn_SelectFile
+            // 
+            this.btn_SelectFile.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_SelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SelectFile.Location = new System.Drawing.Point(47, 46);
+            this.btn_SelectFile.Name = "btn_SelectFile";
+            this.btn_SelectFile.Size = new System.Drawing.Size(100, 35);
+            this.btn_SelectFile.TabIndex = 0;
+            this.btn_SelectFile.Text = "Select File:";
+            this.btn_SelectFile.UseVisualStyleBackColor = false;
+            // 
+            // btn_Parse
+            // 
+            this.btn_Parse.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_Parse.Location = new System.Drawing.Point(47, 401);
+            this.btn_Parse.Name = "btn_Parse";
+            this.btn_Parse.Size = new System.Drawing.Size(100, 26);
+            this.btn_Parse.TabIndex = 1;
+            this.btn_Parse.Text = "Parse";
+            this.btn_Parse.UseVisualStyleBackColor = false;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_Save.Location = new System.Drawing.Point(180, 401);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(100, 26);
+            this.btn_Save.TabIndex = 2;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Case Name:";
+            // 
+            // txb_selectedFile
+            // 
+            this.txb_selectedFile.Location = new System.Drawing.Point(153, 55);
+            this.txb_selectedFile.Name = "txb_selectedFile";
+            this.txb_selectedFile.Size = new System.Drawing.Size(414, 20);
+            this.txb_selectedFile.TabIndex = 4;
+            // 
+            // txb_CaseName
+            // 
+            this.txb_CaseName.Location = new System.Drawing.Point(153, 84);
+            this.txb_CaseName.Name = "txb_CaseName";
+            this.txb_CaseName.Size = new System.Drawing.Size(414, 20);
+            this.txb_CaseName.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 128);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(710, 252);
+            this.dataGridView1.TabIndex = 6;
             // 
             // form_AddData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(712, 451);
+            this.ClientSize = new System.Drawing.Size(843, 461);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txb_CaseName);
+            this.Controls.Add(this.txb_selectedFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Parse);
+            this.Controls.Add(this.btn_SelectFile);
             this.Name = "form_AddData";
             this.Text = "Add Data";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_SelectFile;
+        private System.Windows.Forms.Button btn_Parse;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb_selectedFile;
+        private System.Windows.Forms.TextBox txb_CaseName;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
